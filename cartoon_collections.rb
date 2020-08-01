628367f66194d6f508aa9640b625b88226cf84f5
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-01 15:38:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,7 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
-end
-
+def roll_call_dwarves(dwarfs)# This method should accept an array of dwarf names
+  # It should then print out each name, in number order, using puts
+    dwarfs.each_with_index do |name, index|
+      puts "#{index + 1} #{name}"
+    end
+  end  
+  
 def summon_captain_planet# code an argument here
   # Your code here
 end
