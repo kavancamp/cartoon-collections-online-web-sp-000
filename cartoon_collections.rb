ab4f78fe3776f7faced73b1eaae25680a3953508
Keenah Vancampenhout <kvancamp@mail.com>
2020-08-01 15:57:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ end
   answer
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
     cheese.find do |cheese|
